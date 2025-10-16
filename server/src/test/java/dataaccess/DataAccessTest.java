01cf972502fc277f67d27c633b6dc5118d68a824
Kaija <kaijakaye@gmail.com>
2025-10-16 10:46:02 -0600

    made some tests

---
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -0,0 +1,33 @@
+package dataaccess;
+
+import datamodel.*;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataAccessTest {
+
+    @Test
+    void clear() {
+        DataAccess db = new MemoryDataAccess();
+        db.createUser(new UserData("joe", "j@j.com", "toomanysecrets"));
+        db.clear();
+        assertNull(db.getUser("joe"));
+    }
+
+    @Test
+    void createUser() {
+        DataAccess db = new MemoryDataAccess();
+        var user = new UserData("joe", "j@j.com", "toomanysecrets");
+        db.createUser(user);
+        assertEquals(user, db.getUser(user.username()));
+
+    }
+
+    @Test
+    void getUser() {
+    }
+}
