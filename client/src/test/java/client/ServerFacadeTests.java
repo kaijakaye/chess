fff2253fab2ec71e5fba6735cd8a450c0021e1b5
Kaija <kaijakaye@gmail.com>
2025-11-12 19:28:34 -0700

    finished server facade tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -46,11 +46,27 @@ public class ServerFacadeTests {
     }
 
     @Test
+    void registerFailure() throws Exception {
+        var user = new UserData(null, "password", "p1@email.com");
+        Exception exception = assertThrows(Exception.class, () -> {facade.register(user);});
+        assertEquals("failure: { \"message\": \"Error: bad request\" }", exception.getMessage());
+    }
+
+    @Test
     void loginSuccess() throws Exception {
         UserData userEx = new UserData("player2", "password", "p1@email.com");
         var authData = facade.register(userEx);
         var authData2 = facade.login(userEx);
         assertTrue(authData.authToken().length() > 10);
+    }
+
+    @Test
+    void loginFailure() throws Exception {
+        var user = new UserData("person", "password", "p1@email.com");
+        facade.register(user);
+        var wrongUser = new UserData("person", "wrongPassword", "p1@email.com");
+        Exception exception = assertThrows(Exception.class, () -> {facade.login(wrongUser);});
+        assertEquals("failure: { \"message\": \"Error: unauthorized\" }", exception.getMessage());
     }
 
     @Test
@@ -60,6 +76,17 @@ public class ServerFacadeTests {
 
         // Logging out with a valid token should NOT throw an exception
         assertDoesNotThrow(() -> facade.logout(auth));
+    }
+
+    @Test
+    void logoutFailure() throws Exception {
+        var user = new UserData("person", "password", "p1@email.com");
+        var auth = facade.register(user);
+        var auth2 = facade.login(user);
+        //successful the first time
+        var wrongToken = new AuthData("random-token",user.username());
+        Exception exception = assertThrows(Exception.class, () -> {facade.logout(wrongToken);});
+        assertEquals("failure: { \"message\": \"Error: unauthorized\" }", exception.getMessage());
     }
 
     @Test
@@ -91,6 +118,20 @@ public class ServerFacadeTests {
         var gameStuff = facade.createGame(auth,game);
         var req = new JoinGameRequest(ChessGame.TeamColor.WHITE, gameStuff.getGameID());
         assertDoesNotThrow(() -> facade.joinGame(auth,req));
+    }
+
+    @Test
+    void joinGameFailure() throws Exception {
+        var user = new UserData("logoutUser", "password", "logout@email.com");
+        var auth = facade.register(user);
+        var game = new GameData("myGame");
+
+        var gameStuff = facade.createGame(auth,game);
+        var req = new JoinGameRequest(ChessGame.TeamColor.WHITE, gameStuff.getGameID());
+        assertDoesNotThrow(() -> facade.joinGame(auth,req));
+        //trying to join as a color that's already taken
+        Exception exception = assertThrows(Exception.class, () -> {facade.joinGame(auth,req);});
+        assertEquals("failure: { \"message\": \"Error: already taken\" }", exception.getMessage());
     }
 
     @Test
